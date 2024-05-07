18cb62beb1ab87a855f4650c3b09fcad53b7edad
lakhdher <noureddine.bennejma@insat.u-carthage.tn>
2024-05-08 00:59:48 +0100

    :boom: update project, introduce other model, update gitignore

---
+++ utils.py
@@ -0,0 +1,109 @@
+from operator import truediv
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+import scipy.io as sio
+from sklearn.base import accuracy_score
+from sklearn.decomposition import PCA
+from sklearn.metrics import classification_report, cohen_kappa_score, confusion_matrix
+from sklearn.model_selection import train_test_split
+
+def loadData(name):
+    if name == 'IP':
+        data = sio.loadmat('HSI_dataset/Indian_pines_corrected.mat')['indian_pines_corrected']
+        labels = sio.loadmat('HSI_dataset/Indian_pines_gt.mat')['indian_pines_gt']
+    elif name == 'SA':
+        data = sio.loadmat('HSI_dataset/Salinas_corrected.mat')['salinas_corrected']
+        labels = sio.loadmat('HSI_dataset/Salinas_gt.mat')['salinas_gt']
+    elif name == 'PU':
+        data = sio.loadmat('HSI_dataset/PaviaU.mat')['paviaU']
+        labels = sio.loadmat('HSI_dataset/PaviaU_gt.mat')['paviaU_gt']
+    elif name == 'PC':
+        data = sio.loadmat('Pavia_Dataset/Pavia.mat')['pavia']
+        labels = sio.loadmat('Pavia_Dataset/Pavia_gt.mat')['pavia_gt']
+    
+    return data, labels
+    
+def splitTrainTestSet(X, y, testRatio, randomState=15):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=testRatio, random_state=randomState,
+                                                        stratify=y) #same random split will be generated every time
+    return X_train, X_test, y_train, y_test
+
+def applyPCA(X, numComponents=75):
+    newX = np.reshape(X, (-1, X.shape[2]))
+    pca = PCA(n_components=numComponents, whiten=True)
+    newX = pca.fit_transform(newX)
+    newX = np.reshape(newX, (X.shape[0],X.shape[1], numComponents))
+    return newX, pca
+
+def padWithZeros(X, margin=2):
+    newX = np.zeros((X.shape[0] + 2 * margin, X.shape[1] + 2* margin, X.shape[2]))
+    x_offset = margin
+    y_offset = margin
+    newX[x_offset:X.shape[0] + x_offset, y_offset:X.shape[1] + y_offset, :] = X
+    return newX
+
+def createImageCubes(X, y, windowSize=5, removeZeroLabels = True):
+    margin = int((windowSize - 1) / 2)
+    zeroPaddedX = padWithZeros(X, margin=margin)
+    # split patches
+    patchesData = np.zeros((X.shape[0] * X.shape[1], windowSize, windowSize, X.shape[2]))
+    patchesLabels = np.zeros((X.shape[0] * X.shape[1]))
+    patchIndex = 0
+    for r in range(margin, zeroPaddedX.shape[0] - margin):
+        for c in range(margin, zeroPaddedX.shape[1] - margin):
+            patch = zeroPaddedX[r - margin:r + margin + 1, c - margin:c + margin + 1]   
+            patchesData[patchIndex, :, :, :] = patch
+            patchesLabels[patchIndex] = y[r-margin, c-margin]
+            patchIndex = patchIndex + 1
+    if removeZeroLabels:
+        patchesData = patchesData[patchesLabels>0,:,:,:]
+        patchesLabels = patchesLabels[patchesLabels>0]
+        patchesLabels -= 1
+    return patchesData, patchesLabels
+
+def plot_class_distribution(class_labels):
+    class_counts = pd.Series(class_labels.flatten()).value_counts()
+    class_counts.plot(kind='bar')
+    plt.xlabel('Class')
+    plt.ylabel('Count')
+    plt.title('Class Distribution')
+    plt.show()
+    
+def AA_andEachClassAccuracy(confusion_matrix):
+    counter = confusion_matrix.shape[0]
+    list_diag = np.diag(confusion_matrix)
+    list_raw_sum = np.sum(confusion_matrix, axis=1)
+    each_acc = np.nan_to_num(truediv(list_diag, list_raw_sum))
+    average_acc = np.mean(each_acc)
+    return each_acc, average_acc
+
+def reports(model, X_test,y_test,name):
+    Y_pred = model.predict(X_test)
+    y_pred = np.argmax(Y_pred, axis=1)
+    if name == 'IP':
+        target_names = ['Alfalfa', 'Corn-notill', 'Corn-mintill', 'Corn'
+                        ,'Grass-pasture', 'Grass-trees', 'Grass-pasture-mowed', 
+                        'Hay-windrowed', 'Oats', 'Soybean-notill', 'Soybean-mintill',
+                        'Soybean-clean', 'Wheat', 'Woods', 'Buildings-Grass-Trees-Drives',
+                        'Stone-Steel-Towers']
+    elif name == 'SA':
+        target_names = ['Brocoli_green_weeds_1','Brocoli_green_weeds_2','Fallow','Fallow_rough_plow','Fallow_smooth',
+                        'Stubble','Celery','Grapes_untrained','Soil_vinyard_develop','Corn_senesced_green_weeds',
+                        'Lettuce_romaine_4wk','Lettuce_romaine_5wk','Lettuce_romaine_6wk','Lettuce_romaine_7wk',
+                        'Vinyard_untrained','Vinyard_vertical_trellis']
+    elif name == 'PU':
+        target_names = ['Asphalt','Meadows','Gravel','Trees', 'Painted metal sheets','Bare Soil','Bitumen',
+                        'Self-Blocking Bricks','Shadows']
+    
+    classification = classification_report(np.argmax(y_test, axis=1), y_pred, target_names=target_names)
+    oa = accuracy_score(np.argmax(y_test, axis=1), y_pred)
+    conf = confusion_matrix(np.argmax(y_test, axis=1), y_pred)
+    each_acc, aa = AA_andEachClassAccuracy(conf)
+    kappa = cohen_kappa_score(np.argmax(y_test, axis=1), y_pred)
+    score = model.evaluate(X_test, y_test, batch_size=32)
+    Test_Loss =  score[0]*100
+    Test_accuracy = score[1]*100
+    
+    return classification, conf, Test_Loss, Test_accuracy, oa*100, each_acc*100, aa*100, kappa*100
+
